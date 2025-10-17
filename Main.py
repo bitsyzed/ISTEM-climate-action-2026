388a6d7475eb2b78474ff96e103b0bf37dd2e513
bitsyzed <114426905+bitsyzed@users.noreply.github.com>
2025-10-17 10:25:38 -0400

    Chaning radio.receive to be able to become a string.
    Doing this to make it case insensitive.

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 # Imports
 from microbit import *
 import radio
-
+from typing import Union
 radio.on()
 
 while True:
-    message = radio.receive()
+    message: Union[str,None] = radio.receive()
     
-    if accelerometer.current_gesture() == "freefall":
-        display.show(Image.SAD)
-        radio.send("Falling!")
-    else:
-        display.show(Image.HAPPY)
-    
-    if message:
-        display.scroll(message)
-        sleep(1000)
+    if button_a.was_pressed() or message.lower() == "PRIME":
+        radio.send("Primed.")
+        while True:
+            if accelerometer.current_gesture() == "freefall":
+                display.show(Image.SAD)
+                radio.send("Falling!")
+                sleep(1000)
+                break
+            else:
+                display.show(Image.HAPPY)
+                            
+    display.show(Image.ASLEEP)
