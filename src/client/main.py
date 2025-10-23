a41831c777c02f441bd5effdfecb26f767adcfa1
Jackwastakenx2 <88010263+Jackwastakenx2@users.noreply.github.com>
2025-10-23 08:46:46 -0400

    Update and rename main.py to main.py

--- src/client/main.py
+++ src/client/main.py
@@ -7,17 +7,17 @@ while True:
     message = radio.receive()
     
     if button_a.was_pressed() or str(message).upper() == "PRIME":
-        radio.send("Primed.")
+        radio.send("Primed")
         while True:
             message = radio.receive()
             
             if button_b.was_pressed() or str(message).upper() == "RESET":
-                radio.send("No longer primed.")
+                radio.send("Unprimed")
                 break
             
             if accelerometer.current_gesture() == "freefall":
                 display.show(Image.SAD)
-                radio.send("Falling!")
+                radio.send("Falling")
                 sleep(2500)
                 break
             else:
